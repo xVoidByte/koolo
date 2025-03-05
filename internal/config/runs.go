4ee145b36a13902c9e9b0f207b2ffb724fa5d00d
xVoidByte <george.mihaylov96@gmail.com>
2025-03-05 19:33:53 +0200

    Update runs.go
    Make areas maintainable easier for future development of areas.

--- internal/config/runs.go
+++ internal/config/runs.go
@@ -1,5 +1,9 @@
 package config
 
+import (
+	"github.com/hectorgimenez/d2go/pkg/data/area"
+)
+
 type Run string
 
 const (
@@ -62,4 +66,30 @@ var AvailableRuns = map[Run]interface{}{
 	DrifterCavernRun:    nil,
 	SpiderCavernRun:     nil,
 	EnduguRun:           nil,
+}
+
+var RunAreas = map[Run][]area.ID{
+	CountessRun:       {area.TowerCellarLevel1, area.TowerCellarLevel2, area.TowerCellarLevel3, area.TowerCellarLevel4, area.TowerCellarLevel5},
+	AndarielRun:       {area.CatacombsLevel1, area.CatacombsLevel2, area.CatacombsLevel3, area.CatacombsLevel4},
+	AncientTunnelsRun: {area.AncientTunnels},
+	MausoleumRun:      {area.Mausoleum},
+	SummonerRun:       {area.ArcaneSanctuary},
+	DurielRun:         {area.DurielsLair},
+	MephistoRun:       {area.DuranceOfHateLevel3},
+	TravincalRun:      {area.Travincal},
+	EldritchRun:       {area.FrigidHighlands},
+	PindleskinRun:     {area.NihlathaksTemple},
+	NihlathakRun:      {area.HallsOfAnguish, area.HallsOfPain, area.HallsOfVaught},
+	TristramRun:       {area.Tristram},
+	LowerKurastRun:    {area.LowerKurast},
+	StonyTombRun:      {area.StonyTombLevel1, area.StonyTombLevel2},
+	PitRun:            {area.PitLevel1, area.PitLevel2},
+	ArachnidLairRun:   {area.SpiderCave, area.SpiderCavern},
+	TalRashaTombsRun:  {area.TalRashasTomb1, area.TalRashasTomb2, area.TalRashasTomb3, area.TalRashasTomb4, area.TalRashasTomb5, area.TalRashasTomb6, area.TalRashasTomb7},
+	BaalRun:           {area.ThroneOfDestruction, area.TheWorldstoneChamber},
+	DiabloRun:         {area.ChaosSanctuary},
+	CowsRun:           {area.MooMooFarm},
+	DrifterCavernRun:  {area.DrifterCavern},
+	SpiderCavernRun:   {area.SpiderCavern},
+	EnduguRun:         {area.FlayerJungle},
 }
