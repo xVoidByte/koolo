3c0396ae8d231cc4d2127617b5a19dd8ab3b5b91
xVoidByte <george.mihaylov96@gmail.com>
2025-04-01 20:19:59 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,12 +1,17 @@
 @echo off
+setlocal enabledelayedexpansion
 
 echo Start building Koolo
 echo Cleaning up previous artifacts...
 if exist build rmdir /s /q build > NUL || goto :error
 
+:: Generate unique identifiers
+for /f "delims=" %%a in ('powershell "[guid]::NewGuid().ToString()"') do set "BUILD_ID=%%a"
+for /f "delims=" %%b in ('powershell "Get-Date -Format 'o'"') do set "BUILD_TIME=%%b"
+
 echo Building Koolo binary...
 if "%1"=="" (set VERSION=dev) else (set VERSION=%1)
-go build -trimpath -tags static --ldflags -extldflags="-static" -ldflags="-s -w -H windowsgui -X 'github.com/hectorgimenez/koolo/internal/config.Version=%VERSION%'" -o build/koolo.exe ./cmd/koolo > NUL || goto :error
+garble -literals -tiny -seed=random build -a -trimpath -tags static --ldflags "-s -w -H windowsgui -X 'main.buildID=%BUILD_ID%' -X 'main.buildTime=%BUILD_TIME%' -X 'github.com/hectorgimenez/koolo/internal/config.Version=%VERSION%'" -o "build\%BUILD_ID%.exe" ./cmd/koolo > NUL || goto :error
 
 echo Copying assets...
 mkdir build\config > NUL || goto :error
