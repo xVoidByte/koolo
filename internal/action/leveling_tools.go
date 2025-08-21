dee02dd7bf679cddfa6e0e9fa206aa6ea3961694
Your Name <your.email@example.com>
2025-08-21 22:45:06 +0200

    make-replacing-of-merc-work-in-legacy-graphics

--- internal/action/leveling_tools.go
+++ internal/action/leveling_tools.go
@@ -568,33 +568,32 @@ func HireMerc() error {
             ctx.Logger.Debug("An Act 2 merc is already present, no need to hire a new one.")
             return nil
         }
-        
+
         // Hire the merc if we don't have one, we have enough gold, and we are in act 2.
         if ctx.CharacterCfg.Game.Difficulty == difficulty.Normal && (ctx.Data.MercHPPercent() <= 0 || !isMercenaryPresent(npc.Guard)) && ctx.Data.PlayerUnit.TotalPlayerGold() > 5000 && ctx.Data.PlayerUnit.Area == area.LutGholein {
             ctx.Logger.Info("Hiring merc...")
-            // TODO: Hire Holy Freeze merc if available, if not, hire Defiance merc.
-            err := InteractNPC(town.GetTownByArea(ctx.Data.PlayerUnit.Area).MercContractorNPC())
-            if err != nil {
-                return err
-            }
-            ctx.HID.KeySequence(win.VK_HOME, win.VK_DOWN, win.VK_RETURN)
-            utils.Sleep(2000)
-            if ctx.Data.LegacyGraphics {
-                ctx.HID.Click(game.LeftButton, ui.FirstMercFromContractorListXClassic, ui.FirstMercFromContractorListYClassic)
-                utils.Sleep(50)
-                ctx.HID.Click(game.LeftButton, ui.FirstMercFromContractorListXClassic, ui.FirstMercFromContractorListYClassic)
-            } else {
-                ctx.HID.Click(game.LeftButton, ui.FirstMercFromContractorListX, ui.FirstMercFromContractorListY)
-                utils.Sleep(50)
-                ctx.HID.Click(game.LeftButton, ui.FirstMercFromContractorListX, ui.FirstMercFromContractorListY)
-            }
-            utils.Sleep(2000)
-            ctx.HID.KeySequence(win.VK_HOME, win.VK_RETURN)
+            
+                // TODO: Hire Holy Freeze merc if available, if not, hire Defiance merc.
+                err := InteractNPC(town.GetTownByArea(ctx.Data.PlayerUnit.Area).MercContractorNPC())
+                if err != nil {
+                    return err
+                }
+                ctx.HID.KeySequence(win.VK_HOME, win.VK_DOWN, win.VK_RETURN)
+                utils.Sleep(2000)
+                if ctx.Data.LegacyGraphics {
+                ctx.HID.KeySequence(win.VK_RETURN)
+                } else {
+                    ctx.HID.Click(game.LeftButton, ui.FirstMercFromContractorListX, ui.FirstMercFromContractorListY)
+                    utils.Sleep(50)
+                    ctx.HID.Click(game.LeftButton, ui.FirstMercFromContractorListX, ui.FirstMercFromContractorListY)
+                }
+                utils.Sleep(2000)
+                ctx.HID.KeySequence(win.VK_HOME, win.VK_RETURN)           
         }
     }
 
     return nil
-}
+}}
 
 func ResetStats() error {
 	ctx := context.Get()
