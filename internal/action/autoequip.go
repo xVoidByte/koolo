53a225df6a9c9db2297f8c8c7eceb2e4b28ba265
guiyomu-dev <22749537+guiyomu-dev@users.noreply.github.com>
2025-09-14 10:47:22 +0200

    add khalim's will to the exceptions in autoequip

--- internal/action/autoequip.go
+++ internal/action/autoequip.go
@@ -28,13 +28,13 @@ var (
 	ErrNotEnoughSpace = errors.New("not enough inventory space")
 
 	classItems = map[data.Class][]string{
-		data.Amazon:     {"ajav", "abow", "aspe"},
-		data.Sorceress:  {"orb"},
+		data.Amazon:      {"ajav", "abow", "aspe"},
+		data.Sorceress:   {"orb"},
 		data.Necromancer: {"head"},
-		data.Paladin:    {"ashd"},
-		data.Barbarian:  {"phlm"},
-		data.Druid:      {"pelt"},
-		data.Assassin:   {"h2h"},
+		data.Paladin:     {"ashd"},
+		data.Barbarian:   {"phlm"},
+		data.Druid:       {"pelt"},
+		data.Assassin:    {"h2h"},
 	}
 
 	// shieldTypes defines items that should be equipped in right arm (technically they can be left or right arm but we don't want to try and equip two shields)
@@ -51,6 +51,7 @@ var (
 		"HoradricStaff",
 		"AmuletOfTheViper",
 		"KhalimsFlail",
+		"KhalimsWill",
 	}
 )
 
@@ -465,7 +466,6 @@ func equipBestItems(itemsByLoc map[item.LocationType][]data.Item, target item.Lo
 	return equippedSomething, nil
 }
 
-
 func getBodyLocationScreenCoords(bodyloc item.LocationType) (data.Position, error) {
 	ctx := context.Get()
 	if ctx.Data.LegacyGraphics {
@@ -520,7 +520,6 @@ func getBodyLocationScreenCoords(bodyloc item.LocationType) (data.Position, erro
 	}
 }
 
-
 func equipBestRings(itemsByLoc map[item.LocationType][]data.Item) (bool, error) {
 	ctx := context.Get()
 
@@ -545,7 +544,7 @@ func equipBestRings(itemsByLoc map[item.LocationType][]data.Item) (bool, error) 
 	})
 
 	if len(allRings) == 0 {
-		return false, nil 
+		return false, nil
 	}
 
 	bestRing := allRings[0]
@@ -569,7 +568,7 @@ func equipBestRings(itemsByLoc map[item.LocationType][]data.Item) (bool, error) 
 	for _, equipped := range equippedRings {
 		if equipped.UnitID != 0 && !idealIDs[equipped.UnitID] {
 			ringToReplace = equipped
-			break 
+			break
 		}
 	}
 
@@ -610,8 +609,6 @@ func equipBestRings(itemsByLoc map[item.LocationType][]data.Item) (bool, error) 
 
 	return false, nil
 }
-
-
 
 // equip handles the physical process of equipping an item. Returns ErrNotEnoughSpace if it fails.
 func equip(itm data.Item, bodyloc item.LocationType, target item.LocationType) error {
@@ -656,7 +653,7 @@ func equip(itm data.Item, bodyloc item.LocationType, target item.LocationType) e
 			ctx.HID.PressKeyBinding(ctx.Data.KeyBindings.MercenaryScreen)
 			utils.Sleep(EquipDelayMS)
 			ctx.HID.ClickWithModifier(game.LeftButton, ui.GetScreenCoordsForItem(itm).X, ui.GetScreenCoordsForItem(itm).Y, game.CtrlKey)
-		} else { 
+		} else {
 			currentlyEquipped := GetEquippedItem(ctx.Data.Inventory, bodyloc)
 			isRingSwap := itm.Desc().Type == "ring" && currentlyEquipped.UnitID != 0
 
@@ -669,9 +666,9 @@ func equip(itm data.Item, bodyloc item.LocationType, target item.LocationType) e
 
 				oldRingCoords, err := getBodyLocationScreenCoords(bodyloc)
 				if err != nil {
-					return err 
+					return err
 				}
-				
+
 				ctx.HID.ClickWithModifier(game.LeftButton, oldRingCoords.X, oldRingCoords.Y, game.ShiftKey)
 				utils.Sleep(1000)
 				*ctx.Data = ctx.GameReader.GetData()
@@ -681,7 +678,7 @@ func equip(itm data.Item, bodyloc item.LocationType, target item.LocationType) e
 					ctx.Logger.Warn("Failed to unequip old ring, it is still equipped. Aborting swap.")
 					return fmt.Errorf("failed to unequip old ring from %s", bodyloc)
 				}
-				
+
 				var newItemInInv data.Item
 				var foundInInv bool
 				for _, invItem := range ctx.Data.Inventory.ByLocation(item.LocationInventory) {
@@ -733,7 +730,6 @@ func equip(itm data.Item, bodyloc item.LocationType, target item.LocationType) e
 	}
 	return fmt.Errorf("verification failed after all attempts to equip %s", itm.IdentifiedName)
 }
-
 
 // findInventorySpace finds the top-left grid coordinates for a free spot in the inventory.
 func findInventorySpace(itm data.Item) (data.Position, bool) {
@@ -893,5 +889,4 @@ func UnEquipMercenary() error {
 	}
 
 	return nil
-
 }
