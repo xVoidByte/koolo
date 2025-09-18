9b79089ea334682db188e0fba70b412bd929c9fa
kwader2k <147276807+kwader2k@users.noreply.github.com>
2025-09-18 20:06:14 +0200

    TowerCellarLevel5 Shrine Search Exception

--- internal/action/step/move.go
+++ internal/action/step/move.go
@@ -496,7 +496,7 @@ func findClosestShrine() *data.Object {
 	ctx := context.Get()
 
 	// Check if the bot is dead or chickened before proceeding.
-	if ctx.Data.PlayerUnit.HPPercent() <= 0 || ctx.Data.PlayerUnit.HPPercent() <= ctx.Data.CharacterCfg.Health.ChickenAt || ctx.Data.AreaData.Area.IsTown() {
+	if ctx.Data.PlayerUnit.HPPercent() <= 0 || ctx.Data.PlayerUnit.HPPercent() <= ctx.Data.CharacterCfg.Health.ChickenAt || ctx.Data.AreaData.Area.IsTown() || ctx.Data.AreaData.Area == area.TowerCellarLevel5 {
 		ctx.Logger.Debug("Bot is dead or chickened, skipping shrine search.")
 		return nil
 	}
