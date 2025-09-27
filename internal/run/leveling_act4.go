8456465503360757da1680e3794270bbf6f0e2ac
kwader2k <147276807+kwader2k@users.noreply.github.com>
2025-09-27 21:25:09 +0200

    another oopsie fixed

--- internal/run/leveling_act4.go
+++ internal/run/leveling_act4.go
@@ -57,7 +57,7 @@ func (a Leveling) act4() error {
 
 	lvl, _ := a.ctx.Data.PlayerUnit.FindStat(stat.Level, 0)
 	_, found := a.ctx.Data.Objects.FindOne(object.LastLastPortal)
-	if !found && a.ctx.Data.Quests[quest.Act4TerrorsEnd].Completed() && (lvl.Value >= 60 && a.ctx.CharacterCfg.Game.Difficulty == difficulty.Nightmare && effectiveFireRes >= 75 && effectiveLightRes >= 50) || (lvl.Value >= 33 && a.ctx.CharacterCfg.Game.Difficulty == difficulty.Normal) {
+	if !found && a.ctx.Data.Quests[quest.Act4TerrorsEnd].Completed() && ((lvl.Value >= 60 && a.ctx.CharacterCfg.Game.Difficulty == difficulty.Nightmare && effectiveFireRes >= 75 && effectiveLightRes >= 50) || (lvl.Value >= 33 && a.ctx.CharacterCfg.Game.Difficulty == difficulty.Normal)) {
 		err := action.InteractNPC(npc.Tyrael2)
 		if err != nil {
 			return err // It's good practice to handle errors
